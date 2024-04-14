ef65c48d5ea5e040817034bc69a6f83897347ed3
Javier2992 <123154776+Javier2992@users.noreply.github.com>
2024-04-14 18:19:17 +0200

    l

---
+++ app/src/main/java/com/example/tfg_def/InvitarAmigo.java
@@ -0,0 +1,14 @@
+package com.example.tfg_def;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InvitarAmigo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invitar_amigo);
+    }
+}
--- app/src/main/java/com/example/tfg_def/Perfil.java
+++ app/src/main/java/com/example/tfg_def/Perfil.java
@@ -2,14 +2,38 @@ package com.example.tfg_def;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Perfil extends AppCompatActivity {
 
+    Button invitar, informacion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
+        inicializarVariables();
+        invitar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Perfil.this,
+                        InvitarAmigo.class);
+                startActivity(intent);
+            }
+        });
+        informacion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+    }
+
+    private void inicializarVariables() {
+        invitar = findViewById(R.id.btnInvitar);
+        informacion = findViewById(R.id.btnInfo);
 
     }
 }
